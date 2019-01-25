4220eb8584ad3b4291e34e8cdf960f866cb1ef5b
James-Livesey <jamesliveg@gmail.com>
2019-01-25 18:23:09 +0000

    Add redirection system

---
+++ redirect.js
@@ -0,0 +1,9 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
+$(function() {
+    firebase.database().ref("users/" + getURLParameter("user") + "/public/products/subServer/projects/" + getURLParameter("project") + "/redirect").once("value", function(snapshot) {
+       window.location.href = snapshot.val();
+    });
+});
